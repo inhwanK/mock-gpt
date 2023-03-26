5249dc65266693a15490ea4bf5a9a68ebd879ab8
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-03-27 00:42:24 +0900

    first commit

---
+++ src/main/java/com/inhwan/mockgpt/MockGptApplication.java
@@ -0,0 +1,13 @@
+package com.inhwan.mockgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inhwan/mockgpt/MockGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inhwan.mockgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
