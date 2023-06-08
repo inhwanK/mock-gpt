5604dfa82eee68a75c06e3be1659e7efbe75c771
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-06-08 17:48:51 +0900

    feat: chat completion api 추가

--- src/main/java/com/inhwan/mockgpt/OpenAiApiTestController.java
+++ src/main/java/com/inhwan/mockgpt/OpenAiApiTestController.java
@@ -1,28 +1,59 @@
 package com.inhwan.mockgpt;
 
 import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
 public class OpenAiApiTestController {
 
-    @GetMapping("/test")
-    public String test() {
-        OpenAiService service = new OpenAiService("your_token");
+    //    private final OpenAiService openAiService;
+    private static String apiKey;
+
+    @Value("${secret.key}")
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @GetMapping("/completion")
+    public CompletionResult testCompletion() {
+        OpenAiService service = new OpenAiService(apiKey);
+
         CompletionRequest completionRequest = CompletionRequest.builder()
-                .prompt("Somebody once told me the world is gonna roll me")
+                .prompt("사람을 짜증나게하는 방법 두가지 중 첫번째는 말을 끝까지 하지 않는 것이고, 두번째는 ")
                 .model("ada")
                 .echo(true)
                 .build();
 
-        log.info("?안되냐?");
-        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
-        return "되냐?";
+//        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
+        return service.createCompletion(completionRequest);
+    }
+
+    @GetMapping("/chat/completion")
+    public ChatCompletionResult testChatCompletion() {
+        OpenAiService service = new OpenAiService(apiKey);
+
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model("gpt-3.5-turbo-0301")
+                .temperature(0.8)
+                .n(10)
+                .messages(List.of(new ChatMessage("user", "Create 10 sentences to use as post dummy data in Korean")))
+                .build();
+
+//        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
+        return service.createChatCompletion(chatCompletionRequest);
     }
 }
